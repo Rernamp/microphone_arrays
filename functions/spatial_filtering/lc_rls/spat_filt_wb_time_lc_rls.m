452c4328b6356d55b3c0450af0e3c93cf9b37d40
kuzds <kuznetsov1995dima@mail.ru>
2021-04-17 12:41:13 +0300

    реорганизация

---
+++ functions/spatial_filtering/lc_rls/spat_filt_wb_time_lc_rls.m
@@ -0,0 +1,48 @@
+function [ y,W ] = LC_RLS( s_in, J, M)
+% test commit
+
+    delta_min_2 = 1000;
+lamda = 1;
+%инициализация
+x = zeros(M*J,1);
+R_inv = delta_min_2*eye(M*J,M*J);
+
+f = [1; zeros(J-1,1)];
+
+C = [ones(M,1) ;zeros(M*(J-1),1)];
+for l = 1 : J-1
+    C = [C [zeros(l*M,1) ; ones(M,1) ; zeros(M*(J-l-1),1)]];
+end
+
+Gama = R_inv * C; 
+
+Fi_inv = inv(C' * Gama);
+
+W = Gama * Fi_inv * f;
+y = zeros(1,length(s_in(1,:)));
+for k = 1:length(s_in(1,:))
+    
+    x = [s_in(:,k); x(1:end-M)];
+    y(k) = W' * x;
+    g = (R_inv * x)./(lamda + x' * R_inv * x);
+    R_inv = (R_inv - g * x' * R_inv)./(lamda);
+    
+    nu = C' * g;
+ 
+    n = x' * Gama;
+  
+    
+    om = (Fi_inv * nu)./(1 - n * Fi_inv * nu);
+    
+    Fi_inv = lamda.*(Fi_inv + om * n * Fi_inv);
+    
+    Gama = (Gama - g * n)./(lamda);
+    
+    alpha = 0 - W' * x;
+    
+    W = W + (g - lamda * Gama * om).* conj(alpha);
+    
+end
+
+end
+
